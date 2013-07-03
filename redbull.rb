79fbee759940608cbbfac5a30f4e69571294f109
Tom Chapin <tchapin@gmail.com>
2013-07-03 13:27:42 -0600

    moved event loop into a thread

--- redbull.rb
+++ redbull.rb
@@ -6,6 +6,7 @@ class RedBullAdventure
   def initialize
     @alert_level = rand 25..100
     @money       = rand 1..10
+    @action_thread = Thread.new {}
   end
 
   def drink_red_bull
@@ -25,9 +26,9 @@ class RedBullAdventure
       sleep 2
     else
       sleep 2
-      puts "\nYou go to the gas station to buy a Red Bull.\n"
+      puts "\nYou go to the gas station to buy a Red Bull ($2.00).\n"
       sleep 2
-      puts "\nYou're out of money!\n\n"
+      puts "\nYou don't have enough money!\n\n"
       sleep 2
       "sleep mode... ".chars.each do |char|
         print char
@@ -49,28 +50,39 @@ class RedBullAdventure
       puts "\n\nYou wake up 8 hours later feeling refreshed!\n"
       sleep 1
       @alert_level = 100
-      @money       = rand 1..10
-      puts "\nYou look around and realize people mistook you for a beggar and have given you a total of $#{@money}.00\n"
+      money_given  = rand 1..10
+      @money       += money_given
+      puts "\nYou look around and realize people mistook you for a beggar and have given you a total of $#{money_given}.00\n"
       sleep 2
     end
   end
 
-  def action_loop
-    @alert_level -= 5
-    print "\rAlert Level: #{@alert_level} | Money: $#{@money}.00"
-    sleep 1
-    if @alert_level < 20
-      puts "\n\nYou are coming down off of your caffeine high\nand your legs and arms feel like lead...\n"
-      if @alert_level < 10
-        check_pocket_change
+  def start_action_loop
+    @loop_running = true
+    @action_thread = Thread.new do
+      while @loop_running
+        @alert_level -= 5
+        print "\rAlert Level: #{@alert_level} | Money: $#{@money}.00"
+        sleep 1
+        if @alert_level < 20
+          puts "\n\nYou are coming down off of your caffeine high\nand your legs and arms feel like lead...\n"
+          if @alert_level < 10
+            check_pocket_change
+          end
+          puts "\n"
+        end
       end
-      puts "\n"
     end
-    self.send :action_loop
+    @action_thread.join
+  end
+
+  def stop_action_loop
+    @loop_running = false
+    @action_thread.kill
   end
 
 end
 
 system('clear')
 rba = RedBullAdventure.new
-rba.action_loop
+rba.start_action_loop
